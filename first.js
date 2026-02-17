dee818de0f45a2950303a36251029d5090e8a1ee
Aditya Singh <adityasingh998466@gmail.com>
2026-02-17 12:10:35 +0530

    updated

---
+++ first.js
@@ -0,0 +1,14 @@
+// const person=()=>{
+//     const name = "aditya";
+//     const age = 21;
+//     const city ="gorakhpur";
+//     return "name :" + name + "age :"+age +"city :"+city;
+// }
+// export default person;
+const person=()=>{
+    const name="aditya";
+    const age=21;
+    const city="ghaziabad";
+    return"Name: " + name + "Age: " + age + "City: " + city;
+}
+export default person;
